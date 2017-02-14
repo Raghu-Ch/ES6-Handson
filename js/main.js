0ddde3bf1bb91c26505ec0f42c41b7c4149a03c0
Raghu-Ch <raghu6004@gmail.com>
2017-02-13 23:41:00 -0800

    Using arrow functions and setting up webpack

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let calculateMonthlyPayment = function (principal, years, rate) {
+let calculateMonthlyPayment = (principal, years, rate) => {
     let monthlyRate = 0;
     if (rate) {
         monthlyRate = rate / 100 / 12;
@@ -13,12 +13,31 @@ let calculateMonthlyPayment = function (principal, years, rate) {
     //      monthlyPayment: monthlyPayment,
     //      monthlyRate: monthlyRate };
 };
+let calculateAmortization = (principal, years, rate) => {
+    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
+    let balance = principal;
+    let amortization = [];
+    for (let y=0; y<years; y++) {
+        let interestY = 0; // Interest payment for year y
+        let principalY = 0; // principal payment for year y
+        for (let m=0; m<12; m++) {
+            let interestM = balance * monthlyRate; // Interest payment for month m
+            let principalM = monthlyPayment - interestM; //principal payment for month m
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+        amortization.push({principalY, interestY, balance})
+    }
+    return {monthlyPayment, monthlyRate, amortization};
+};
 
-document.getElementById('calcBtn').addEventListener('click', function () {
+document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate, amortization} = calculateAmortization(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate*100).toFixed(2);
+    amortization.forEach(month => console.log(month));
 });
